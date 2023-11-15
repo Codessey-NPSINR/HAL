248a7be52060f888756e0cc130d4360b0dd98bff
pritamghiremath <150879226+pritamghiremath@users.noreply.github.com>
2023-11-15 12:27:42 +0530

    Add files via upload
    Commit 3 - added user control for which location they are choosing

--- mapview - Copy.py
+++ mapview - Copy.py
@@ -70,9 +70,11 @@ def get_coordinates_mapquest_custom(address, api_key):
 
 # Example usage:
 api_key = '3NrvEGQ7qy9w9KibqNcGLZHSLUEjnvCO'
-#ad = input('Enter address: ')
-ad = 'indiranagar, bangalore, india'
+ad = input('Enter address: ')
 coordinates = get_coordinates_mapquest_custom(ad, api_key)
+
+colat = coordinates[0]
+colong = coordinates[1]
 
 if coordinates:
     print(f"Coordinates for {ad}: {coordinates}")
@@ -87,9 +89,15 @@ bounding_box = get_bounding_box(latitude, longitude, radius_km)
 print("Bounding Box:", bounding_box)
 
 
+
 root = tk.Tk()
 root.geometry('1000x700')
 root.title('Map view')
+
+map_widget = tkintermapview.TkinterMapView(root, width = 1400, height = 700, corner_radius=10)
+map_widget.place(relx=0.5, rely=0.5,anchor=tk.CENTER)
+map_widget.set_position(colat, colong)
+map_widget.set_zoom(11)
 
 
 minlat = bounding_box[0]
@@ -97,14 +105,14 @@ minlong = bounding_box[1]
 maxlat = bounding_box[2]
 maxlong = bounding_box[3]
 
-marker_ind = map_widget.set_marker(12.97394, 77.6439, text="Point", marker_color_circle='blue', marker_color_outside='green')
+marker_ind = map_widget.set_marker(colat, colong, text="Point", marker_color_circle='blue', marker_color_outside='green')
 
-marker_1 = map_widget.set_marker(12.928973919704065, 77.3319710696493, text="A", marker_color_circle='blue', marker_color_outside='green')
+marker_1 = map_widget.set_marker(minlat, minlong, text="A", marker_color_circle='blue', marker_color_outside='green')
 
-marker_2 = map_widget.set_marker(12.928973919704065, 77.9558289303507, text="B", marker_color_circle='blue', marker_color_outside='green')
+marker_2 = map_widget.set_marker(minlat, maxlong, text="B", marker_color_circle='blue', marker_color_outside='green')
 
-marker_3 = map_widget.set_marker(13.018906080295936, 77.9558289303507, text="C", marker_color_circle='blue', marker_color_outside='green')
+marker_3 = map_widget.set_marker(maxlat, maxlong, text="C", marker_color_circle='blue', marker_color_outside='green')
 
-marker_4 = map_widget.set_marker(13.018906080295936, 77.3319710696493, text="D", marker_color_circle='blue', marker_color_outside='green')
+marker_4 = map_widget.set_marker(maxlat, minlong, text="D", marker_color_circle='blue', marker_color_outside='green')
 
 root.mainloop()
